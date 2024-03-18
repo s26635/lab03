b70b9e0720958bcf7d7efb412c257cc55b2a514e
s26635 <s26635@pjwstk.edu.pl>
2024-03-18 11:13:45 +0100

    Container and OverfillException addded

---
+++ lab03/lab03/Exceptions/OverfillException.cs
@@ -0,0 +1,9 @@
+﻿namespace lab03;
+
+public class OverfillException : Exception
+{
+    public OverfillException()
+    {
+        Console.Error.WriteLine("Masa ładunku przekracza maksymalną ładowność!");
+    }
+}
---
+++ lab03/lab03/Models/Base/Container.cs
@@ -0,0 +1,39 @@
+﻿namespace lab03;
+
+public class Container
+{
+    protected float LoadWeigth { get; set; }
+    protected float Heigth { get; set; }
+    protected float ContainerWeigth { get; set; }
+    protected float Depth { get; set; }
+    protected string SerialNumber { get; set; }
+    protected float MaxLoad { get; set; }
+
+    public Container(float loadWeigth, float heigth, float containerWeigth, float depth, string serialNumber,
+        float maxLoad)
+    {
+        LoadWeigth = loadWeigth;
+        Heigth = heigth;
+        ContainerWeigth = containerWeigth;
+        Depth = depth;
+        SerialNumber = serialNumber;
+        MaxLoad = maxLoad;
+    }
+
+    public void Deload()
+    {
+        LoadWeigth = 0;
+    }
+
+    public void Load(float LoadWeigth)
+    {
+        if (LoadWeigth > MaxLoad)
+        {
+            throw new OverfillException();
+        }
+        else
+        {
+            LoadWeigth = LoadWeigth;
+        }
+    }
+}
